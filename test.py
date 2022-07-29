7efdbcecbc079db4c733d6bf245881eb7f89fded
Karan-Mokashi <109713949+Karan-Mokashi@users.noreply.github.com>
2022-07-29 17:14:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -30,12 +30,12 @@ q1=data['Quantity'].quantile(0.25)
 q2=data['Quantity'].quantile(0.50)
 q3=data['Quantity'].quantile(0.75)
 iqr=q3-q1
-iqr
+#iqr
 
 
 upper_limit=q3+1.5*iqr
 lower_limit=q1-1.5*iqr
-upper_limit,lower_limit
+#upper_limit,lower_limit
 
 
 #replacing upper values with upper limit and lower values with lower limit
